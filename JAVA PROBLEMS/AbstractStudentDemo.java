6bd70425b9e7c544a7f4f370dfe8e42712bee5ef
Dwaipayan Majaumder <24052003@kiit.ac.in>
2026-01-15 16:24:37 +0530

    c

--- JAVA PROBLEMS/AbstractStudentDemo.java
+++ JAVA PROBLEMS/AbstractStudentDemo.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+abstract class Student
+{
+    int rollno;
+    long regno;
+    void getinput(int r, long reg)
+    {
+        rollno = r;
+        regno = reg;
+    }
+    abstract void course();
+}
+class Kiitian extends Student
+{
+    @Override
+    void course()
+    {
+        System.out.println("Course - B.Tech. (Computer Science & Engg)");
+    }
+}
+public class AbstractStudentDemo
+{
+    public static void main(String[] args)
+    {
+        Kiitian k = new Kiitian();
+        k.getinput(2205180, 1234567890L);
 
-public class MAIN {
+        System.out.println("Rollno - " + k.rollno);
+        System.out.println("Registration no - " + k.regno);
+        k.course();
+    }
 }
